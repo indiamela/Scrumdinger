1e0425bf8ef1d63b227f5046c46356789d656733
Taishi Kusunose <taishi.kusunose@gmail.com>
2021-11-21 17:06:01 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Taishi Kusunose on 2021/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
